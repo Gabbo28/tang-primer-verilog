8fa4720b513069e4b74c556e5c506e6c2a1c6c14
gabriele bocca <b.gabri@gmail.com>
2024-08-10 23:46:46 +0200

    removed tx references, added .cst file and top module

--- sim/top_tb.v
+++ sim/top_tb.v
@@ -1,14 +1,12 @@
 `timescale 1ns/1ps
 
-module tx_tb();
+module top_tb();
 
-reg tb_clk = 1'b1;
-reg tb_en = 1'b0;
 
-uart_tx txi (
+proj_module mod_inst (
     .clk(tb_clk),
-    .data_in(8'hAA),
-    .en(tb_en)
+    .data_in(),
+    .data_out()
 );
 
 always
@@ -19,17 +17,14 @@ end
 initial
 begin
     #100
-    tb_en <= 1'b1;
-    #100
-    tb_en <= 1'b0;
-    #200000
+    ...
     $finish;
 end
 
 initial
 begin
-    $dumpfile("tx_tb.vcd");
-    $dumpvars(1,txi);
+    $dumpfile("top_tb.vcd");
+    $dumpvars;
 end
 
 endmodule
